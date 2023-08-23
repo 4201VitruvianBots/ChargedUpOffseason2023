7ce4ca714d83d5b72efae060ed85579ab8e81ec7
Ronan B <rdavidbouza@gmail.com>
2023-08-23 13:23:35 -0700

    Hashmap WIP

--- src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -4,5 +4,30 @@
 
 package frc.robot.subsystems;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.ctre.phoenixpro.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.CAN;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utils.CtreUtils.Devices.CANCoder;
+import frc.robot.utils.SwerveUtils.SWERVE_MODULE_POSITION;
+
 /** Add your docs here. */
-public class SwerveDrive {}
+public class SwerveDrive extends SubsystemBase implements AutoCloseable{
+
+    Private final HashMap<SWERVE_MODULE_POSITION, SwerveModule> m_swerveModules =
+        new HashMap<>(
+            Map.of(
+                SWERVE_MODULE_POSITION.FRONT_LEFT, 
+                new SwerveModule(
+                    SWERVE_MODULE_POSITION.FRONT_LEFT,
+                    new TalonFX(CAN.frontLeftTurnMotor),
+                    new TalonFX(CAN.frontLeftDriveMotor),
+                    new CANCoder(CAN.frontLeftCanCoder),
+                    SWERVE_DRIVE.frontLeftCANCoderOffset),
+
+                
+            ) );
+}
